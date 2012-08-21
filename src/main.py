daf8a3470570df535c2bfd5e65a35616e4ad2596
Radosław Szalski <radoslaw.szalski@gmail.com>
2012-08-22 00:02:06 +0200

    Extracts basic billing information line by line (only page 6).

--- src/main.py
+++ src/main.py
@@ -5,10 +5,13 @@
 Author: Radosław Szalski
 '''
 
+from __future__ import print_function
 import sys
+import re
+from pprint import pprint
 
 from pdfminer.converter import PDFPageAggregator
-from pdfminer.layout import LTFigure, LTImage, LTTextBox, LTTextLine, LAParams
+from pdfminer.layout import LTFigure, LTImage, LTTextBox, LTTextLine, LAParams, LTText, LTChar, LTLine, LTAnon
 from pdfminer.pdfparser import PDFDocument, PDFParser
 from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
 from pdfminer.pdfdevice import PDFDevice
@@ -30,13 +33,86 @@ def parsePDF(PDFPath):
         device = PDFPageAggregator(rsrcmgr, laparams=laparams)
         interpreter = PDFPageInterpreter(rsrcmgr, device)
 
-        for page in doc.get_pages():
+        for pageNumber, page in enumerate(doc.get_pages(), start=1):
             interpreter.process_page(page)
-            layout = device.get_result()
+            pageLayout = device.get_result()
 
-            for lt_obj in layout._objs:
-                if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
-                    print(lt_obj.get_text())
+            if pageNumber == 6:
+                content = parseBilling(pageLayout)
+                printBilling(content)
+
+
+def printBilling(textLines):
+    for line in textLines:
+        if ':' in line:
+            print(line)
+
+def parseBilling(pageLayout):
+    from collections import defaultdict
+
+    columns = {}
+    columns['left'] = defaultdict(lambda: {})
+    columns['right'] = defaultdict(lambda: {})
+    finalLines = []
+
+    lastUsedX, lastUsedY = 0, 0
+    lastUsedSide = ''
+
+    dividerCoordinate = 310.393
+
+    for char in getChars(pageLayout):
+        if hasattr(char, 'bbox'):
+            (x, y, x1, y1) = char.bbox
+
+            # Rounding up to avoid quirks like chars misaligned by one thousandth of a pt
+            y = int(y)
+            y1 = int(y1)
+
+            text = char.get_text()
+
+            # Small font, used for seconds, is 5 pts in height
+            # We want it to go on the same line as minutes (7 pt)
+            if y1 - y == 5:
+                y = y - 2
+
+            if x < dividerCoordinate:
+                columns['left'][y][x] = text
+                lastUsedSide = 'left'
+            else:
+                columns['right'][y][x] = text
+                lastUsedSide = 'right'
+
+            lastUsedY = y
+            lastUsedX = x
+        else:
+            print('X: %f, Y: %f' % (lastUsedX, lastUsedY))
+            if char.get_text() == '\n':
+                columns[lastUsedSide][lastUsedY][lastUsedX + 0.000001] = ' '
+            else:
+                columns[lastUsedSide][lastUsedY][lastUsedX + 0.000001] = text
+
+    for column in columns:
+        for lineNumber in sorted(columns[column].keys(), reverse=True):
+            line = ''
+
+            for charPos in sorted(columns[column][lineNumber].keys()):
+                char = columns[column][lineNumber][charPos]
+                line += char
+
+            finalLines.append(line)
+
+    return finalLines
+
+def getChars(pageLayout):
+    '''
+    Chars include actual characters as well as LTAnon objects (spaces, non-printable).
+    '''
+    for layoutObject in pageLayout._objs:
+        if isinstance(layoutObject, LTText):
+            for textLine in layoutObject._objs:
+                for char in textLine._objs:
+                    if isinstance (char, LTChar) or isinstance(char, LTAnon):
+                        yield char
 
 if __name__ == '__main__':
     try:
